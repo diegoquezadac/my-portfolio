b86bb0144d8c990610907cb6ff00ee986fcdfd7e
Diego Quezada <diego.quezadac@sansano.usm.cl>
2020-12-28 13:55:40 -0300

    gh page and react added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> My portfolio </h1>
+      <h1> My portfolio </h1>
+      <h2> Author: Diego Quezada</h2>
+    </div>
+  );
+}
+
+export default App;
